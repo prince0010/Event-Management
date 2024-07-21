dfa2b24fefefeb76ae090603d5ad22297b32e2dd
PRINCE <princenagac12@gmail.com>
2024-07-21 22:46:11 +0800

    Make a robust and flexible API, Added Resources and added some validations in the Event routes and added a protected fillable in the Event Model

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\EventResource;
 use App\Models\Event;
 use Illuminate\Http\Request;
 
@@ -13,9 +14,10 @@ class EventController extends Controller
      */
     public function index()
     {
-        return Event::all();
+        // return EventResource::collection(Event::all());
 
-
+        // This will be loading all the events together in the database with the user relationship
+        return EventResource::collection(Event::with('user', 'attendees')->get());
     }
 
     /**
@@ -23,7 +25,17 @@ class EventController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $event = Event::create([
+            ...$request->validate([
+                'name'=> 'required|string|max:255',
+                'description'=>'nullable|string',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_time'
+            ]),
+            'user_id' => 1
+        ]);
+
+        return new EventResource($event);
     }
 
     /**
@@ -31,22 +43,40 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        return $event;
+        // load the user in the event
+        $event->load('user', 'attendees');
+        return new EventResource($event);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Event $event)
     {
-        //
+      $event->update(
+        $request->validate([
+            'name'=> 'sometimes|string|max:255',
+            'description'=>'nullable|string',
+            'start_time' => 'sometimes|date',
+            'end_time' => 'sometimes|date|after:start_time'
+        ]));
+
+        return new EventResource($event);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Event $event)
     {
-        //
+        $event->delete();
+    
+        // So when delete a resource, we should not be returning the resource because it doesn't exist anymore.
+        // In latest versions of PHP, you can use named parameters, so we can just do status for no content is 204. = status: 204
+        // REMEMBER: You dont send any body with the response when you pass the status as 204 because it means that no content is available. And a common practice if you delete resources
+        return response(status: 204);
+        
+        // Old version of php which doesn't support named parameters is like this:
+        // return response(' ' , 204);
     }
 }
---
+++ app/Http/Resources/EventResource.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EventResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'description' => $this->description,
+            'start_time' => $this->start_time,
+            'end_time' => $this->end_time,
+            // So User is basically the organizer of the event, and when using this magical when loaded method of the JsonResource, what will happen is this user property would only be --
+            // -- present on the response if this user relationship of particular event is loaded.
+            'user'=> new UserResource($this->whenLoaded('user')),
+            // You can use this whenLoaded() to load one single relation or even a collection of resources. Events has user() and attendees()
+            // We use Collection() method because we actually list the attendees and then we can do as well this when loaded attendees. 
+            // The attendees here is a static method so no new instance is created.
+            'attendees'=> AttendeesResource::collection($this->whenLoaded('attendees'))
+        ];
+        
+    }
+    // php artisan make:resource EventResource
+
+}
